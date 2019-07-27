38448f7f7a1c10c625a48fb112ed8c42c02a2e4a
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-07-27 15:42:33 -0700

    added goal header function to give color

--- Development Files/Mastery_iOS/Mastery_iOS/AddGoalDetailsViewController.swift
+++ Development Files/Mastery_iOS/Mastery_iOS/AddGoalDetailsViewController.swift
@@ -14,6 +14,11 @@ class AddGoalDetailsViewController: UIViewController {
     var purpose:  String?
     var purposeTwo: String?
     var purposeThree: String?
+    var colorArray: [UIColor] = [UIColor(red:0.49, green:0.47, blue:0.73, alpha:1.0),
+                                 UIColor(red:0.91, green:0.44, blue:0.32, alpha:1.0),
+                                 UIColor(red:0.35, green:0.76, blue:0.76, alpha:1.0),
+                                 UIColor(red:0.55, green:0.70, blue:0.41, alpha:1.0),
+                                 UIColor(red:0.96, green:0.74, blue:0.38, alpha:1.0)]
     
     @IBOutlet weak var dateOfBirth: UIDatePicker!
     @IBOutlet weak var deadline: UIDatePicker!
@@ -51,9 +56,35 @@ class AddGoalDetailsViewController: UIViewController {
         goal.hoursEstimate = 10.0
         goal.hoursCompleted = 0.0
         goal.priority = 1
+        //        goal.color = colorArray[Int(arc4random_uniform(UInt32(colorArray.count)))]
+        goal.color = getColorFromUserDefaults(colorArray: colorArray)
         
         PersistenceService.saveContext()
        return goal
     }
 
+    
+    
+    func getColorFromUserDefaults(colorArray: [UIColor]) -> UIColor {
+        if let colorIndex  = UserDefaults.standard.object(forKey: "goalColorIndex") as? Int {
+            if colorIndex == colorArray.count - 1 {
+                UserDefaults.standard.set(0, forKey: "goalColorIndex")
+                return colorArray[0]
+            } else {
+                UserDefaults.standard.set(colorIndex + 1, forKey: "goalColorIndex")
+                return colorArray[colorIndex + 1]
+            }
+        }
+        else {
+            UserDefaults.standard.set(0, forKey: "goalColorIndex")
+            return colorArray[0]
+        }
+    }
+    
 }
+
+
+
+
+
+
